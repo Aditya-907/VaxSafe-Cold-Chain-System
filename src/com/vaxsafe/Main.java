391fe7235933e17c3f66076013730271332be29e
Aditya Singh Tomar <adityasinght970@gmail.com>
2026-03-25 22:25:54 +0530

    test(core): add basic main method to validate model functionality

--- src/com/vaxsafe/Main.java
+++ src/com/vaxsafe/Main.java
@@ -1,5 +1,24 @@
 package com.vaxsafe;
 
+import com.vaxsafe.model.VaccineBatch;
+
+import java.time.LocalDate;
+
 public class Main {
+    public static void main(String[] args) {
+        VaccineBatch batch = new VaccineBatch(
+            "BATCH001",
+            "Peraxine",
+            LocalDate.of(2026,12,31),
+            2.0,
+            8.0
+        );
+
+        System.out.println("Batch ID: " + batch.getBatchId());
+        System.out.println("Vaccine: " + batch.getVaccineName());
+        System.out.println("Is it Safe Temperature(5°C): " + batch.isTemperatureSafe(5.0));
+        System.out.println("Is Expired: " + batch.isExpired());
+    }
+
     
 }
