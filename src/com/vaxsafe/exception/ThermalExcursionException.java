e505675200e404fb64af90d299ebe63392b8b235
Aditya Singh Tomar <adityasinght970@gmail.com>
2026-03-26 18:54:58 +0530

    feat(exception): add thermal excursion exception for unsafe temperature handling

---
+++ src/com/vaxsafe/exception/ThermalExcursionException.java
@@ -0,0 +1,9 @@
+package com.vaxsafe.exception;
+
+public class ThermalExcursionException extends Exception{
+
+    //Makes exception with message
+    public ThermalExcursionException(String message){
+        super(message);
+    }
+}
