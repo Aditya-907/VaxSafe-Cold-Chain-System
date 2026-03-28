a4c2ab49b576bb03751ff1b17ddd491607974536
Aditya Singh Tomar <adityasinght970@gmail.com>
2026-03-28 21:25:11 +0530

    feat(core): integrate service layer with simulation for real-time monitoring

--- src/com/vaxsafe/Main.java
+++ src/com/vaxsafe/Main.java
@@ -1,6 +1,8 @@
 package com.vaxsafe;
 
 import com.vaxsafe.model.VaccineBatch;
+import com.vaxsafe.service.AlertSystem;
+import com.vaxsafe.service.ColdChainManager;
 import com.vaxsafe.simulation.EnvironmentSimulator;
 
 import java.time.LocalDate;
@@ -13,20 +15,23 @@ public class Main {
     public static void main(String[] args) {
 
         VaccineBatch batch1 = new VaccineBatch(
-                "BATCH101", "Foxin",
+                "BATCH201", "Jorex",
                 LocalDate.of(2026, 12, 31),
                 2.0, 8.0
         );
 
         VaccineBatch batch2 = new VaccineBatch(
-                "BATCH102", "Covishield",
+                "BATCH202", "Rovet",
                 LocalDate.of(2026, 11, 30),
                 2.0, 8.0
         );
 
         List<VaccineBatch> batches = Arrays.asList(batch1, batch2);
 
-        EnvironmentSimulator simulator = new EnvironmentSimulator(2);
+        AlertSystem alertSystem = new AlertSystem();
+        ColdChainManager manager = new ColdChainManager(alertSystem);
+
+        EnvironmentSimulator simulator = new EnvironmentSimulator(2, manager);
 
         simulator.startSimulation(batches);
     }
