280205cefb08d435258d52371373c67875fd08ad
Aditya Singh Tomar <adityasinght970@gmail.com>
2026-03-29 23:20:09 +0530

    feat(core): integrate database persistence with simulation and service layers

--- src/com/vaxsafe/Main.java
+++ src/com/vaxsafe/Main.java
@@ -1,5 +1,6 @@
 package com.vaxsafe;
 
+import com.vaxsafe.dao.InventoryDAO;
 import com.vaxsafe.model.VaccineBatch;
 import com.vaxsafe.service.AlertSystem;
 import com.vaxsafe.service.ColdChainManager;
@@ -9,29 +10,38 @@ import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
-// Entry point to validate sensor simulation
+// Entry point wiring all system components
 public class Main {
 
     public static void main(String[] args) {
 
+        InventoryDAO dao = new InventoryDAO();
+        dao.createTable();
+
         VaccineBatch batch1 = new VaccineBatch(
-                "BATCH201", "Jorex",
+                "BATCH301", "Covaxin",
                 LocalDate.of(2026, 12, 31),
                 2.0, 8.0
         );
 
         VaccineBatch batch2 = new VaccineBatch(
-                "BATCH202", "Rovet",
+                "BATCH302", "Covishield",
                 LocalDate.of(2026, 11, 30),
                 2.0, 8.0
         );
 
+        // Persist initial batches
+        dao.save(batch1);
+        dao.save(batch2);
+
         List<VaccineBatch> batches = Arrays.asList(batch1, batch2);
 
         AlertSystem alertSystem = new AlertSystem();
-        ColdChainManager manager = new ColdChainManager(alertSystem);
+        ColdChainManager manager =
+                new ColdChainManager(alertSystem, dao);
 
-        EnvironmentSimulator simulator = new EnvironmentSimulator(2, manager);
+        EnvironmentSimulator simulator =
+                new EnvironmentSimulator(2, manager);
 
         simulator.startSimulation(batches);
     }
